21d7cabcfb264d158c227780fd91eeb0ee761513
dmitriy.khlopkov <dmitriy.khlopkov@arcadia.spb.ru>
2019-03-04 18:21:09 +0300

    CORS configured

--- ArcadiaTest/App_Start/WebApiConfig.cs
+++ ArcadiaTest/App_Start/WebApiConfig.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using Unity;
 
 namespace ArcadiaTest
@@ -25,6 +26,10 @@ namespace ArcadiaTest
             container.RegisterType<IUserService, UserService>();
             container.RegisterType<ITaskService, TaskService>();
             config.DependencyResolver = new DependencyResolver(container);
+
+            // Configuring CORS
+            var cors = new EnableCorsAttribute("http://localhost:4200", "*", "*");
+            config.EnableCors(cors);
 
             // Web API routes
             config.MapHttpAttributeRoutes();
