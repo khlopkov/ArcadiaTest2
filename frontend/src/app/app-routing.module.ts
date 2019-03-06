ce4298a3113ec7401ce4c795c7a812a6c3e92785
dmitriy.khlopkov <dmitriy.khlopkov@arcadia.spb.ru>
2019-03-06 14:04:28 +0300

    added navigation panel for main page and routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TasksComponent } from './tasks/tasks.component';
+import { TaskModule } from './tasks/tasks.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: TasksComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [TaskModule, RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  declarations: [],
 })
 export class AppRoutingModule { }
