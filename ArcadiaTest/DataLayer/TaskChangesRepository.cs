090e3ccbe2c8216571db8f7587b91a0e64e16339
dmitriy.khlopkov <dmitriy.khlopkov@arcadia.spb.ru>
2019-03-14 16:26:34 +0300

    Changed entities to DTOs in parameters and return values of methods of TaskChanges repository

--- ArcadiaTest/DataLayer/TaskChangesRepository.cs
+++ ArcadiaTest/DataLayer/TaskChangesRepository.cs
@@ -16,9 +16,24 @@ namespace ArcadiaTest.DataLayer
             this._dbCtx = dbCtx;
         }
 
-        public IEnumerable<TaskChange> FindChangesByTaskID(int taskId)
+        private TaskChangeDTO taskChangeEntityToDTO(TaskChange entity)
         {
-            return this._dbCtx.TaskChanges.Where(tc => tc.TaskId == taskId).ToList();
+            return entity == null ? null :
+                new TaskChangeDTO()
+                {
+                    Id = entity.Id,
+                    ChangedAt = entity.ChangedAt,
+                    NewValue = entity.NewValue,
+                    OldValue = entity.OldValue,
+                    Operation = entity.Operation,
+                    Task = entity.Task,
+                    TaskId = entity.TaskId
+                };
+        }
+
+        public IEnumerable<TaskChangeDTO> FindChangesByTaskID(int taskId)
+        {
+            return this._dbCtx.TaskChanges.Where(tc => tc.TaskId == taskId).ToList().ToDtos();
         }
 
         public IEnumerable<TaskChangeDTO> FindChangesByUserId(int userId)
@@ -34,6 +49,33 @@ namespace ArcadiaTest.DataLayer
                 Task = this._dbCtx.Tasks.Where(t => t.Id == tc.TaskId).FirstOrDefault(),
             })
                 .Where(tc => tc.Task.UserId == userId).ToList();
+        }
+    }
+
+    public static class TaskChangeExtension
+    {
+        public static TaskChangeDTO ToDto(this TaskChange entity)
+        {
+            return entity == null ? null :
+                new TaskChangeDTO()
+                {
+                    Id = entity.Id,
+                    ChangedAt = entity.ChangedAt,
+                    NewValue = entity.NewValue,
+                    OldValue = entity.OldValue,
+                    Operation = entity.Operation,
+                    Task = entity.Task,
+                    TaskId = entity.TaskId
+                };
+        }
+        public static IEnumerable<TaskChangeDTO> ToDtos(this IReadOnlyCollection<TaskChange> entities)
+        {
+            var taskChanges = new List<TaskChangeDTO>(entities.Count);
+            foreach (var entity in entities)
+            {
+                taskChanges.Add(entity.ToDto());
+            }
+            return taskChanges;
         }
     }
 }
