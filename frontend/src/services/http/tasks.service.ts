a09e9e031589bfc4f95b74cc7dd6adb0a823e119
dmitriy.khlopkov <dmitriy.khlopkov@arcadia.spb.ru>
2019-03-18 17:00:54 +0300

    replaced concatinating baseUrl with prefix to one variable

--- frontend/src/services/http/tasks.service.ts
+++ frontend/src/services/http/tasks.service.ts
@@ -13,32 +13,33 @@ const PREFIX = 'api/user/tasks';
 @Injectable()
 export class TasksService {
 
-    private baseUrl: string = REST_URL;
+    private baseUrlWithPrefix: string = REST_URL + PREFIX;
+
     constructor(
         private http: HttpClient
     ) { }
 
     get(): Observable<Task[]> {
-        return this.http.get<Task[]>(this.baseUrl + PREFIX, { headers: new HttpHeaders(JSON_MIME) });
+        return this.http.get<Task[]>(this.baseUrlWithPrefix, { headers: new HttpHeaders(JSON_MIME) });
     }
 
     post(task: Task) {
-        return this.http.post(this.baseUrl + PREFIX, task, { headers: new HttpHeaders(JSON_MIME) });
+        return this.http.post(this.baseUrlWithPrefix, task, { headers: new HttpHeaders(JSON_MIME) });
     }
 
     patch(task: Task) {
-        return this.http.patch(this.baseUrl + PREFIX + `/${task.id}`, task, { headers: new HttpHeaders(JSON_MIME) });
+        return this.http.patch(this.baseUrlWithPrefix + `/${task.id}`, task, { headers: new HttpHeaders(JSON_MIME) });
     }
 
     delete(task: Task) {
-        return this.http.delete(this.baseUrl + PREFIX + `/${task.id}`);
+        return this.http.delete(this.baseUrlWithPrefix + `/${task.id}`);
     }
 
     dashboard(): Observable<Dashboard> {
-        return this.http.get<Dashboard>(this.baseUrl + PREFIX + `/dashboard/byStatus`, { headers: new HttpHeaders(JSON_MIME)});
+        return this.http.get<Dashboard>(this.baseUrlWithPrefix + `/dashboard/byStatus`, { headers: new HttpHeaders(JSON_MIME)});
     }
 
     history(): Observable<TaskChange[]> {
-        return this.http.get<TaskChange[]>(this.baseUrl + PREFIX + '/history', { headers: new HttpHeaders(JSON_MIME)});
+        return this.http.get<TaskChange[]>(this.baseUrlWithPrefix + '/history', { headers: new HttpHeaders(JSON_MIME)});
     }
 }
