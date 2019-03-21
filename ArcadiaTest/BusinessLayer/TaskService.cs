d8cb6eceb02db927c102cc035de105829c0d47a1
dmitriy.khlopkov <dmitriy.khlopkov@arcadia.spb.ru>
2019-03-21 12:18:47 +0300

    added async methods to TaskService

--- ArcadiaTest/BusinessLayer/TaskService.cs
+++ ArcadiaTest/BusinessLayer/TaskService.cs
@@ -7,6 +7,7 @@ using ArcadiaTest.Models.Requests;
 using ArcadiaTest.Models.Responses;
 using ArcadiaTest.BusinessLayer.Exceptions;
 using ArcadiaTest.Models.DTO;
+using System.Threading.Tasks;
 
 namespace ArcadiaTest.BusinessLayer
 {
@@ -50,6 +51,26 @@ namespace ArcadiaTest.BusinessLayer
             this._taskRepository.Save(task);
         }
 
+        public async Task CreateTaskAsync(int userId, CreateTaskRequest payload)
+        {
+            if (payload == null)
+                throw new ArgumentNullException(nameof(payload));
+
+            if (await this._userRepository.FindUserByIDAsync(userId) == null)
+                throw new UserNotFoundException(userId);
+
+            var task = new TaskDTO
+            {
+                UserId = userId,
+                Title = payload.Title,
+                Description = payload.Description,
+                Status = ACTIVE,
+                Type = payload.Type,
+                DueDate = payload.DueDate,
+            };
+            await this._taskRepository.SaveAsync(task);
+        }
+
         public void DeleteTask(int taskId)
         {
             var task = this._taskRepository.FindTaskById(taskId);
@@ -57,6 +78,15 @@ namespace ArcadiaTest.BusinessLayer
                 throw new TaskNotFoundException(taskId);
 
             this._taskRepository.Delete(task);
+        }
+
+        public async Task DeleteTaskAsync(int taskId)
+        {
+            var task = await this._taskRepository.FindTaskByIdAsync(taskId);
+            if (task == null)
+                throw new TaskNotFoundException(taskId);
+
+            await this._taskRepository.DeleteAsync(task);
         }
 
         private TaskDTO FindTaskDtoById(int id)
@@ -68,14 +98,38 @@ namespace ArcadiaTest.BusinessLayer
             return taskDto;
         }
 
+        private async Task<TaskDTO> FindTaskDtoByIdAsync(int id)
+        {
+            var taskDto = await this._taskRepository.FindTaskByIdAsync(id);
+            if (taskDto == null)
+                throw new TaskNotFoundException(id);
+
+            return taskDto;
+        }
+
         public TaskResponse GetTask(int id)
         {
             return this.FindTaskDtoById(id).ToResponse();
         }
 
+        public async Task<TaskResponse> GetTaskAsync(int id)
+        {
+            var taskDto = await this.FindTaskDtoByIdAsync(id);
+            return taskDto.ToResponse();
+        }
+
         public TaskResponse GetTaskOfUser(int userId, int taskId)
         {
             var taskDto = this.FindTaskDtoById(taskId);
+            if (taskDto == null || taskDto.UserId != userId)
+                throw new TaskNotFoundException();
+
+            return taskDto.ToResponse();
+        }
+
+        public async Task<TaskResponse> GetTaskOfUserAsync(int userId, int taskId)
+        {
+            var taskDto = await this.FindTaskDtoByIdAsync(taskId);
             if (taskDto == null || taskDto.UserId != userId)
                 throw new TaskNotFoundException();
 
@@ -88,12 +142,27 @@ namespace ArcadiaTest.BusinessLayer
             return taskDtos.Select(t => t.ToResponse());
         }
 
+        private async Task<IEnumerable<TaskResponse>> GetTasksOfUserAsync(int userId)
+        {
+            var taskDtos = await this._taskRepository.FindTasksByUserIdAsync(userId);
+            return taskDtos.Select(t => t.ToResponse());
+        }
+
         public IEnumerable<TaskResponse> GetTasksOfUser(int userId, string status = null)
         {
             if (status == null)
                 return this.GetTasksOfUser(userId);
 
             var taskDtos = this._taskRepository.FindTasksByUserIdAndStatus(userId, status);
+            return taskDtos.Select(t => t.ToResponse());
+        }
+
+        public async Task<IEnumerable<TaskResponse>> GetTasksOfUserAsync(int userId, string status = null)
+        {
+            if (status == null)
+                return this.GetTasksOfUser(userId);
+
+            var taskDtos = await this._taskRepository.FindTasksByUserIdAndStatusAsync(userId, status);
             return taskDtos.Select(t => t.ToResponse());
         }
 
@@ -124,6 +193,35 @@ namespace ArcadiaTest.BusinessLayer
                 null : updateModel.Type;
 
             this._taskRepository.Update(taskDto);
+        }
+
+        public async Task UpdateTaskAsync(int id, MergeTaskRequest updateModel)
+        {
+            if (updateModel == null)
+                throw new ArgumentNullException(nameof(updateModel));
+
+            var taskDto = await this._taskRepository.FindTaskByIdAsync(id);
+            if (taskDto == null)
+                throw new TaskNotFoundException(id);
+
+            if (taskDto.Status != ACTIVE)
+                throw new TaskNotActiveException();
+
+            taskDto.Title = !String.IsNullOrEmpty(updateModel.Title) ? updateModel.Title : taskDto.Title;
+
+            taskDto.Description = updateModel.Description == "" ?
+                 null : updateModel.Description;
+
+            taskDto.DueDate = updateModel.DueDate == null ?
+                null : updateModel.DueDate;
+
+            taskDto.Status = updateModel.Status == "" ?
+                taskDto.Status = null : updateModel.Status;
+
+            taskDto.Type = updateModel.Type == "" ?
+                null : updateModel.Type;
+
+            await this._taskRepository.UpdateAsync(taskDto);
         }
     }
 
